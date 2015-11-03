9d50f7a8bf34828dafca9ee48c150c6434049788
Geoffrey <geoffreyplichard@gmail.com>
2015-11-03 22:25:06 +0100

    Update config.xml to fix camera crash

--- front/scripts/login/controllers/profile.controller.js
+++ front/scripts/login/controllers/profile.controller.js
@@ -5,11 +5,12 @@ module.exports = function(app) {
     var fullname = app.name + '.' + controllername;
     /*jshint validthis: true */
 
-    var deps = ['$rootScope', 'main.login.RegistrationService', '$state'];
+    var deps = ['$rootScope', 'main.login.RegistrationService', '$state', '$cordovaCamera'];
 
-    function controller($rootScope, RegistrationService, $state) {
+    function controller($rootScope, RegistrationService, $state, $cordovaCamera) {
         var vm = this;
         vm.controllername = fullname;
+        vm.upload = upload;
 
         activate();
 
@@ -17,6 +18,25 @@ module.exports = function(app) {
             if(!RegistrationService.isAuth()) {
                 $state.go('app.home');
             }
+        }
+
+        function upload() {
+            var options = {
+                quality: 75,
+                destinationType: Camera.DestinationType.DATA_URL,
+                sourceType: Camera.PictureSourceType.CAMERA,
+                allowEdit: true,
+                encodingType: Camera.EncodingType.JPEG,
+                popoverOptions: CameraPopoverOptions,
+                targetWidth: 300,
+                targetHeight: 300,
+                saveToPhotoAlbum: false
+            };
+            $cordovaCamera.getPicture(options).then(function(imageData) {
+                console.log(imageData);
+            }, function(error) {
+                console.log(error);
+            });
         }
 
     }
